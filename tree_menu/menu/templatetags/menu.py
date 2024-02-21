26854cb70fa42ad0a2ad7c0bc4c33ab574d9963d
Эмилар Локтев <emilar-l@yandex.ru>
2024-02-21 16:07:45 +0300

    One query rough

--- tree_menu/menu/templatetags/menu.py
+++ tree_menu/menu/templatetags/menu.py
@@ -31,42 +31,68 @@ def create_layer(
     return layer
 
 
+def get_section(section_id, all_sections) -> Section:
+    for section in all_sections:
+        if section.id == section_id:
+            return section
+
+
 def menu_generator(
         all_sections: QuerySet,
         current_section: Section,
         current_children: dict[str: Section, str: list],
         parent_sections: list[Section],
+        children_of_sections: dict[Section, list[Section]]
         ) -> list[Section, list[Section, list]]:
-    parent = current_section.parent
-    if not parent:
+    parent_id = current_section.parent_id
+    if not parent_id:
         return create_layer(
             parent_sections, current_section, current_children)
 
     menu_layer = create_layer(
-        current_layer_sections=all_sections.filter(parent=parent),
+        current_layer_sections=children_of_sections.get(parent_id, []),
         current_section=current_section,
         current_children=current_children
     )
-    return menu_generator(all_sections, parent, menu_layer, parent_sections)
+    return menu_generator(
+        all_sections,
+        get_section(parent_id, all_sections),
+        menu_layer,
+        parent_sections,
+        children_of_sections
+    )
 
 
 @register.inclusion_tag('menu/menu.html', takes_context=True)
 def draw_menu(context: RequestContext, menu_name: str) -> str:
-    current_url = context.request.path
     all_sections = Section.objects.filter(menu__title=menu_name)
-    parent_sections = [section for section in all_sections.filter(parent=None)]
-    current_section = all_sections.get(url__endswith=current_url)
+
+    parent_sections = []
+    children_of_sections = {}
+
+    for section in all_sections:
+        if section.url == context.request.build_absolute_uri():
+            current_section = section
+        if not section.parent_id:
+            parent_sections.append(section)
+        else:
+            if children_of_sections.get(section.parent_id, None):
+                children_of_sections[section.parent_id].append(section)
+            else:
+                children_of_sections[section.parent_id] = [section]
+
     current_children = [
         {
             'section': section
-        } for section in all_sections.filter(parent=current_section)
+        } for section in children_of_sections.get(current_section.id, [])
     ]
 
     sections = menu_generator(
         all_sections=all_sections,
         current_section=current_section,
         current_children=current_children,
-        parent_sections=parent_sections
+        parent_sections=parent_sections,
+        children_of_sections=children_of_sections
     )
 
     final = {'sections': sections}
